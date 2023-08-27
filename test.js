00563ef78e1bac338b3dc5e637fc7d7444b7efac
JobaHD <92954275+JobaHD@users.noreply.github.com>
2023-08-27 15:26:27 -0400

    git done bby

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 // Your code here
+const { Employee } = require('./employee');
+
+const newEmployee = new Employee('John Wick', 'Dog Lover');
+
+const sayName = newEmployee.sayName.bind(newEmployee);
+
+setTimeout(sayName, 2000);
+
+const sayOccupation = newEmployee.sayOccupation.bind(newEmployee);
+setTimeout(sayOccupation, 3000);
+
